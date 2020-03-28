148f415dfa08e6ea789974eec951f18de06db135
BoD <BoD@JRAF.org>
2020-03-28 15:25:08 +0100

    Fixed a few bugs and improved logging

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(
-    function() {
-        log("BoD's Bookmark Tool v1.0.1");
+    function () {
+        log("BoD's Bookmark Tool v1.0.2");
 
         chrome.alarms.onAlarm.addListener(
-            function(alarm) {
+            function (alarm) {
                 log("Alarm triggered");
                 syncFolders();
             }
